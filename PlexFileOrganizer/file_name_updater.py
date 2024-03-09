d9156a85f9cf6628257749e5bfee58a130622863
ColorlessSaber <nuns-pastes-0s@icloud.com>
2024-03-08 17:01:35 -0800

    Started file_name_updater.py
    Started to create the file_name_updater.py that will update a list of files.

---
+++ PlexFileOrganizer/file_name_updater.py
@@ -0,0 +1,10 @@
+
+
+def file_name_updater(file_list):
+    """
+    A function that takes in a list of files that need their name changed.
+
+    :param file_list: A tuple list were each entry is a file with their old name and their new name.
+    :return: N/A
+    """
+    print('hello world')
