63e386a0c5a8b9c681fa26f9bb2f633d4806cd68
ColorlessSaber <nuns-pastes-0s@icloud.com>
2024-05-12 15:07:24 -0700

    bug fix -- better media file check
    Creating a fuction to do a more indepth job of checking if the media file(s) in the selected directory have been updated or not. For the way it is now, the program can be fooled if only a handful of the files have been updated for a TV show. If it's the case, need to inform the user of the situation.

---
+++ PlexFileOrganizer/functions/media_file_check.py
@@ -0,0 +1,15 @@
+
+def media_file_check(media_file_list, directory):
+    """
+    Checks to see if the media file(s) in the directory have been updated or not.
+
+    :param media_file_list: The list of media file(s) in the directory
+    :param directory: Location of the media files
+    :return: Bool - False for media files not updated; True for media files have been updated
+    """
+    media_files_in_directory_updated = False
+
+    print('hi')
+
+    return media_files_in_directory_updated
+
