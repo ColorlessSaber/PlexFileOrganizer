906b1a7b218c614befd0aa17abf7f7ce3c8fef5e
ColorlessSaber <nuns-pastes-0s@icloud.com>
2025-02-22 20:56:43 -0800

    Restructured scan_directory_thread.py
    Updated the structure of the scan_directory_thread.py to fit with the new directory_scanner generator and the in-progress correct_media_file_format function.

--- PlexFileOrganizer/functions/__init__.py
+++ PlexFileOrganizer/functions/__init__.py
@@ -6,3 +6,4 @@ from .file_name_updater import file_name_updater
 from .media_file_check import media_file_check
 from .custom_objects import MediaFile, ExtraFolders
 from .directory_scanner import directory_scanner
+from .correct_media_file_format import correct_media_file_format
--- PlexFileOrganizer/threads/scan_directory_thread.py
+++ PlexFileOrganizer/threads/scan_directory_thread.py
@@ -1,10 +1,9 @@
 """
 Thread for scanning the directory user selected
 """
-import re
+from collections import deque
 from PySide6 import QtCore as qtc
-from PlexFileOrganizer.functions import directory_scanner
-from PlexFileOrganizer.functions import MediaFile
+from PlexFileOrganizer.functions import directory_scanner, correct_media_file_format, MediaFile
 
 class ThreadSignals(qtc.QObject):
     """
@@ -18,7 +17,7 @@ class ScanDirectoryThread(qtc.QRunnable):
 
     def __init__(self, directory_path):
         super().__init__()
-        self.folders_to_check = [directory_path]
+        self.directory_path = deque([directory_path])
         self.mutex = qtc.QMutex()
         self.signals = ThreadSignals()
 
@@ -29,32 +28,11 @@ class ScanDirectoryThread(qtc.QRunnable):
 
         :return:
         """
-        check_media_file_tv_show_formate = re.compile(r"""
-        ^.+   # wildcard to handle name of show
-        \s  
-        -
-        \s
-        s\d+e\d+(-e\d+)?  # season, episode number and possible multiple episode
-        \.\w+ # file extension
-        """, re.VERBOSE | re.IGNORECASE)
         media_file_list = []    # holds the media files that need to be updated
 
         self.mutex.lock()
-        while self.folders_to_check:    # keep iterating through the folders until there is no more to check
-            for entry in directory_scanner(self.folders_to_check[0]):
-                if entry.is_dir():
-                    #print('directory! {}'.format(entry.name))  # debugging
-                    self.folders_to_check.append(entry.path)
-                elif entry.is_file():
-                    #print('file! {}'.format(entry.name))   # debugging
-                    if any(entry.name.endswith(extension) for extension in ['.mkv', '.mp4', '.avi']):
-                        if re.match(r'Season\s\d', entry.path.split('/')[-2]):  # media folder is a tv show
-                            if not check_media_file_tv_show_formate.match(entry.name):  # if media file is not formated for show, add it to the list
-                                media_file_list.append(MediaFile(entry.path))
+        for entry in directory_scanner(self.directory_path):
+            if not correct_media_file_format(entry.path):
+                media_file_list.append(entry.path)
 
-                        else:   # media folder is movie
-                            if entry.path.split('/')[-2] in entry.name: # if media file is not formated for movie, add it to the list
-                                media_file_list.append(MediaFile(entry.path))
-
-            self.folders_to_check.pop(0)    # remove the last folder that was checked.
         self.mutex.unlock()
