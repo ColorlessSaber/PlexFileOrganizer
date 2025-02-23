49794449258f156f28ce99f6fa5582942b6a7183
ColorlessSaber <nuns-pastes-0s@icloud.com>
2025-02-22 20:54:25 -0800

    Finished directory_scanner.py
    Updated the directory_scanner to be recursive, calling upon itself every time it finds a new folder. While yielding any files it finds along the way.

--- PlexFileOrganizer/functions/directory_scanner.py
+++ PlexFileOrganizer/functions/directory_scanner.py
@@ -2,14 +2,15 @@ import os
 
 def directory_scanner(directory_path):
     """
-    A generator function that steps through the directory path given and returns one entry at a time
-    what is finds.
+    A generator function that drills into a directory with multiple directories, subdirectories, etc.
+    Yields any files it finds.
 
-    :param directory_path: The directory path that will be scanned
+    :param directory_path: The directory path that will be scanned.
     :return: a DirEntry that contains two entries -- path and name.
     """
-
     with os.scandir(directory_path) as directory_to_scan:
         for entry in directory_to_scan:
-            if not entry.name.startswith("."):  # skips over hidden folders/files.
+            if entry.is_dir():
+                directory_scanner(entry.path)
+            elif entry.name.startswith('.') and entry.is_file():
                 yield entry
