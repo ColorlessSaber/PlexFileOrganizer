da3e68d66ecb00de57c43a2850fcc529a9c6f34c
ColorlessSaber <nuns-pastes-0s@icloud.com>
2025-12-12 13:16:47 -0800

    Updated scan_media_folder function
    Added in code to not count the 'Special' season folder when counting how many seasons there are in the tv show media folder.

--- src/functions/scan_media_folder.py
+++ src/functions/scan_media_folder.py
@@ -25,7 +25,9 @@ def scan_media_folder(media_folder_path: str) -> tuple[MediaFolderData, bool]:
                 if folder_and_file_patterns.extra_folder_check(entry.name):
                     media_folder_information.extra_folders[entry.name.lower()] = True
                 elif folder_and_file_patterns.tv_show_season_folder_check(entry.name):
-                    media_folder_information.number_of_seasons += 1
+                    # don't want to count the 'Special' season folder
+                    if not entry.name.lower() == 'Special':
+                        media_folder_information.number_of_seasons += 1
                 else:
                     pass
             else:
