aae7c08f96add7b663840f933280c3f6d402aa00
ColorlessSaber <nuns-pastes-0s@icloud.com>
2025-12-12 12:58:27 -0800

    dynamic button enabling for modified media folder
    Made it to where when the user clicks the button to modified existing media folder it will disable all buttons while the process is being worked on. Once process is down the buttons are re-enabled and the window stays open to allow user to modify another media folder if they wish.

--- src/pop_up_windows/modified_media_folder_window.py
+++ src/pop_up_windows/modified_media_folder_window.py
@@ -13,9 +13,8 @@ class ModifiedMediaFolderWindow(qtw.QDialog):
 
     def __init__(self, parent=None):
         """
-        Dialog window to allow user to select an existing Media Folder and then after the scan of the folder, select
-        what new folders to add to it--Extra folders and or another TV show season folder--or remove folders from the
-        Media Folder.
+        Dialog window to allow user to select an existing Media Folder and add more folders--extra or season folders--
+        to it.
 
         :param parent: The parent window the dialog window will be linked to.
         """
@@ -73,17 +72,17 @@ class ModifiedMediaFolderWindow(qtw.QDialog):
         self.btn_update.setEnabled(False)
         self.btn_update.clicked.connect(self.start_folder_modification)
 
-        btn_cancel = qtw.QPushButton('Cancel', self)
-        btn_cancel.clicked.connect(self.reject)
+        self.btn_cancel = qtw.QPushButton('Cancel', self)
+        self.btn_cancel.clicked.connect(self.reject)
 
         # Set up the layout of window
         main_layout = qtw.QVBoxLayout()
         main_layout.addLayout(select_directory_layout)
         main_layout.addLayout(media_inform_form)
-        main_layout.addLayout(extra_folder_layout)
         main_layout.addLayout(self.season_inform_form)
+        main_layout.addLayout(extra_folder_layout)
         main_layout.addWidget(self.btn_update)
-        main_layout.addWidget(btn_cancel)
+        main_layout.addWidget(self.btn_cancel)
         self.setLayout(main_layout)
 
     @qtc.Slot(object)
@@ -174,7 +173,7 @@ class ModifiedMediaFolderWindow(qtw.QDialog):
     @qtc.Slot()
     def start_folder_modification(self) -> None:
         """
-        Create the object to hold the user's selection and sends it off to be
+        Create the object to hold the user's selection and disable all button widgets before sending it off.
         """
         modified_media_folder_info = ModifyMediaFolder()
         modified_media_folder_info.directory = self.select_directory_label.text()
@@ -191,13 +190,15 @@ class ModifiedMediaFolderWindow(qtw.QDialog):
         modified_media_folder_info.extra_folders['shorts'] = self.cb_shorts.isChecked() if self.cb_shorts.isEnabled() else False
         modified_media_folder_info.extra_folders['other'] = self.cb_other.isChecked() if self.cb_other.isEnabled() else False
 
+        self._enable_or_disable_buttons(False)
+
         self.signal_media_folder_update_information.emit(modified_media_folder_info)
 
     @qtc.Slot()
     def messagebox_folder_not_media_folder(self) -> None:
         """
         Launches the messagebox to inform user the folder is not media folder.
-        Will reset the progress bar once user closes the messagebox window.
+        Will reset the progress bar and enable the button widgets.
 
         :return:
         """
@@ -209,12 +210,13 @@ class ModifiedMediaFolderWindow(qtw.QDialog):
 
         if response == qtw.QMessageBox.Ok:
             self.signal_reset_progress_bar.emit()
+            self._enable_or_disable_buttons(True)
 
     @qtc.Slot()
     def messagebox_media_folder_modification_complete(self) -> None:
         """
         Launches a messagebox to inform user the update of the media folder is complete.
-        Will reset the progress bar and close "modified media folder" window once user closes the messagebox window.
+        Will reset the progress bar, reset the information in the window, and enable the button widgets.
 
         :return:
         """
@@ -226,4 +228,45 @@ class ModifiedMediaFolderWindow(qtw.QDialog):
 
         if response == qtw.QMessageBox.Ok:
             self.signal_reset_progress_bar.emit()
-            self.accept()
+            self._clear_scanned_media_folder_information()
+            self._enable_or_disable_buttons(True)
+
+    def _clear_scanned_media_folder_information(self) -> None:
+        """
+        Clears the information that was scanned from the media folder.
+
+        :return:
+        """
+        self.select_directory_label.setText("")
+        self.media_title.setText("")
+        self.media_type.setText("")
+        self.highest_season_number.setText("")
+        self.number_of_new_seasons.setValue(0)
+        self.cb_trailers.setChecked(False)
+        self.cb_behind_the_scenes.setChecked(False)
+        self.cb_deleted_scenes.setChecked(False)
+        self.cb_featurettes.setChecked(False)
+        self.cb_shorts.setChecked(False)
+        self.cb_interviews.setChecked(False)
+        self.cb_scenes.setChecked(False)
+        self.cb_other.setChecked(False)
+
+
+    def _enable_or_disable_buttons(self, enable_or_disable: bool) -> None:
+        """
+        Enables / disables the button widgets.
+
+        :param enable_or_disable: Sets the enable / disable status
+        :return:
+        """
+        self.btn_select_directory.setEnabled(enable_or_disable)
+        self.btn_cancel.setEnabled(enable_or_disable)
+        self.btn_update.setEnabled(enable_or_disable)
+        self.cb_trailers.setEnabled(enable_or_disable)
+        self.cb_behind_the_scenes.setEnabled(enable_or_disable)
+        self.cb_deleted_scenes.setEnabled(enable_or_disable)
+        self.cb_featurettes.setEnabled(enable_or_disable)
+        self.cb_shorts.setEnabled(enable_or_disable)
+        self.cb_interviews.setEnabled(enable_or_disable)
+        self.cb_scenes.setEnabled(enable_or_disable)
+        self.cb_other.setEnabled(enable_or_disable)
