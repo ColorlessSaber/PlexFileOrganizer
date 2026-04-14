92996a7d622fc007289dbd6ff6ca2faa323cf4fb
ColorlessSaber <nuns-pastes-0s@icloud.com>
2026-04-13 18:22:20 -0700

    updated progress numbers
    For the update_existing_media_folder_thread, updated the progress numbers to make more sense.

--- src/threads/update_existing_media_folder_thread.py
+++ src/threads/update_existing_media_folder_thread.py
@@ -27,7 +27,7 @@ class UpdateExistingMediaFolderThread(qtc.QRunnable):
         """
         try:
             self.signals.progress.emit(
-                13,
+                10,
                 "Starting to update existing media folder: "
                 + self.info_of_media_folder.media_title,
             )
@@ -35,47 +35,47 @@ class UpdateExistingMediaFolderThread(qtc.QRunnable):
             if self.info_of_media_folder.media_type.is_tv():
                 if self.info_of_media_folder.number_of_new_seasons > 0:
                     self.signals.progress.emit(
-                        26, "-- Generating more season folder(s)."
+                        20, "-- Generating more season folder(s)."
                     )
                     self.info_of_media_folder.generate_new_season_folders()
-                    self.signals.progress.emit(39, "-- New season folder(s) generated.")
+                    self.signals.progress.emit(30, "-- New season folder(s) generated.")
                 else:
                     self.signals.progress.emit(
-                        39, "-- No new season folder(s) needed to be created."
+                        30, "-- No new season folder(s) needed to be created."
                     )
 
                 if self.info_of_media_folder.specials_season:
                     self.signals.progress.emit(
-                        52, "-- Generating Specials season folder."
+                        40, "-- Generating Specials season folder."
                     )
                     self.info_of_media_folder.generate_specials_season_folder()
                     self.signals.progress.emit(
-                        65, "-- Specials season folder generated."
+                        50, "-- Specials season folder generated."
                     )
                 else:
                     self.signals.progress.emit(
-                        65, "-- Skipping Specials season folder generation."
+                        50, "-- Skipping Specials season folder generation."
                     )
             else:
                 self.signals.progress.emit(
-                    65,
+                    50,
                     "-- Media folder is for movie; no seasons and/or Specials season folder will be generated.",
                 )
 
             if self.info_of_media_folder.check_if_new_extra_folders_are_needed():
-                self.signals.progress.emit(78, "-- Generating extra folder(s).")
+                self.signals.progress.emit(60, "-- Generating extra folder(s).")
                 self.info_of_media_folder.generate_new_extra_folders()
-                self.signals.progress.emit(91, "-- Extra folder(s) generated.")
+                self.signals.progress.emit(70, "-- Extra folder(s) generated.")
             else:
                 self.signals.progress.emit(
-                    91, "-- No new extra folder(s) needed to be created."
+                    70, "-- No new extra folder(s) needed to be created."
                 )
 
             # renaming of the folder and its contents is done last so it doesn't mess up adding new folders
             if not self.info_of_media_folder.new_media_title.isspace() and (self.info_of_media_folder.new_media_title != ""):
                 self.signals.progress.emit(
-                    15,
-                    "-- Renaming media folder from {} to {}".format(
+                    80,
+                    "-- Renaming media folder from {} to {}, along with files inside folder".format(
                         self.info_of_media_folder.media_title, self.info_of_media_folder.new_media_title
                     )
                 )
@@ -85,9 +85,14 @@ class UpdateExistingMediaFolderThread(qtc.QRunnable):
                     self.info_of_media_folder.new_media_title,
                     self.info_of_media_folder.directory,
                 )
+
+                self.signals.progress.emit(
+                    90,
+                    "-- Renaming of media folder and media files complete"
+                    )
             else:
                 self.signals.progress.emit(
-                    15,
+                    90,
                     "-- Media folder name will be kept."
                 )
 
